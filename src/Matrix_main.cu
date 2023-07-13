a351b3623d9e0e68eadbdb2a5bb06540fd0dea9b
Like2021 <1026569947@qq.com>
2023-07-13 16:53:24 +0800

    update 共享内存实现矩阵乘法

--- src/Matrix_main.cu
+++ src/Matrix_main.cu
@@ -6,6 +6,7 @@
 #define N 512
 
 #define BLOCK_SIZE 32 
+#define width 512
 
 void initial(float *array, int size)
 {
@@ -62,6 +63,49 @@ __global__ void multiplicateMatrixOnDevice(float* array_A, float* array_B, float
     }
 }
 
+__global__ void multiplicateMatrixShareMemory(float* Md, float* Nd, float* Pd)
+{
+    // 开辟共享内存
+    __shared__ float Mds[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float Nds[BLOCK_SIZE][BLOCK_SIZE];
+
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // 标识计算结果Pd对应的行列，这样就确认好了矩阵单个元素对应的线程
+    int Row = by * BLOCK_SIZE + ty;
+    int Col = bx * BLOCK_SIZE + tx;
+
+    float Pvalue = 0;
+
+    // 注: 同一BLOCK_SIZE内的块相加后的结果才是对应的Pd单元素的结果
+    // 这里利用for循环，计算同一BLOCK_SIZE内的块
+    // width / BLOCK_SIZE对应块的数目
+    for (int m = 0; m < width / BLOCK_SIZE; m++)
+    {
+        // 这里对应
+        // Mds[ty][tx] = Md[Row][tx]
+        // Nds[ty][tx] = Nd[ty][Col]
+        // 但全局内存中二维数组是以一维数组的形式保存的，共享内存则不一样，所以这里需要对Md和Nd进行索引转换
+        // 实现从两个矩阵中各取一个元素存入共享内存
+        Mds[ty][tx] = Md[Row * width + (m * BLOCK_SIZE + tx)];
+        Nds[ty][tx] = Nd[(m * BLOCK_SIZE + ty) * width + Col];
+        // 等待所有线程都将对应元素存入共享内存中
+        __syncthreads();
+
+        // 累加块相乘后的子集
+        for (int k = 0; k < BLOCK_SIZE; k++)
+        {
+            Pvalue += Mds[ty][k] + Nds[k][tx];
+        }
+        // 同步结果，保证完全计算完当前块后，再进下一个for循环，计算下一个块
+    }
+    // 最后把结果写入全局内存Pd中
+    Pd[Row * width + Col] = Pvalue;
+}
+
 int main(int argc, char* argv[])
 {
     // 1.组织数据
@@ -88,11 +132,14 @@ int main(int argc, char* argv[])
 	cudaMemcpy(d_B, h_B, Bxy * sizeof(float), cudaMemcpyHostToDevice);
 
     // 3.组织线程配置，调用核函数
-    int dimx = 2;
-    int dimy = 2;
-    dim3 block(dimx, dimy);
-    dim3 grid((M + block.x - 1) / block.x, (N + block.y - 1) / block.y);
-    multiplicateMatrixOnDevice<<<grid,block>>> (d_A, d_B, d_C, M, K, N);
+    // int dimx = 2;
+    // int dimy = 2;
+    // dim3 block(dimx, dimy);
+    // dim3 grid((M + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+    // multiplicateMatrixOnDevice<<<grid, block>>> (d_A, d_B, d_C, M, K, N);
+    dim3 grid(width / BLOCK_SIZE, width / BLOCK_SIZE);
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    multiplicateMatrixShareMemory<<<grid, block>>> (d_A, d_B, d_C);
     cudaMemcpy(deviceRef, d_C, Cxy * sizeof(float), cudaMemcpyDeviceToHost);
     printMatrix(deviceRef, M, N);
 
