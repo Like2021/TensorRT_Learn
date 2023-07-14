30fed518ac9435c2771788a7136170c3a667d4d8
Like2021 <1026569947@qq.com>
2023-07-14 20:20:29 +0800

    update 更新了cublas和cudnn学习

--- src/CublasTest.cu
+++ src/CublasTest.cu
@@ -1,7 +1,22 @@
 #include "cublas_v2.h"
+#include <cuda_runtime.h>
+
+#define M 512
+#define N 512
+#define K 512
+
+#define RUNTIME_CUDA_ERROR(errorInfo) CHECK_CUDA(errorInfo, cudaSuccess)
+
+#define CUDA_FREE(d_ptr){\
+    if (d_ptr != nullptr) RUNTIME_CUDA_ERROR(cudaFree(d_ptr)); d_ptr = nullptr;\
+}\
 
 int main(int argc, char* argv[])
 {
+    // 组织数据
+    int A = M * N;
+
+
     cublasHandle_t handle;
     cublasCreate(&handle);
 
